4e75b34142ff2ef57c67ec1de887adf527d4bf0b
Sean Lee <freshdried@gmail.com>
2017-09-02 17:36:39 -0400

    yeah boi

--- tidal-start.sc
+++ tidal-start.sc
@@ -1,12 +1,11 @@
 s.options.numBuffers = 1024 * 16;
 s.options.memSize = 1024 * 512;
 
-s.waitForBoot{
-Routine{
-include("SuperDirt");
-2.wait;
-SuperDirt.start;
-}.play
-}
-
-// credits to amindfv
+s.waitForBoot {
+  Routine {
+    include("SuperDirt");
+    2.wait;
+    SuperDirt.start;
+  }.play
+};
+// s.dumpOSC();
--- tidalAsLibrary.hs
+++ tidalAsLibrary.hs
@@ -18,53 +18,41 @@ runOnce index computation = do
       return x
     Just store -> ForeignStore.readStore (ForeignStore.Store index)
 
-newtype Func a = Func {appFunc :: a -> a }
-instance Monoid (Func a) where
-  mempty = Func id
-  mappend (Func f) (Func g) = Func (g . f)
-
 main :: IO ()
 main = do
   (cps, d1,t1) <- runOnce 0 $ do
     (cps, getNow) <- bpsUtils
     (d1, t1) <- superDirtSetters getNow
     return (cps, d1, t1)
-  cps (65/60)
-  d1 $ prog silence
+  cps (80/60)
+  d1 $ prog silence 
 
-add x y = stack [x, y]
+add x y = overlay x y
 addMod f x = add (f x) x
 appendMod f x = append' (f x) x
 catMod mods x = slowcat (fmap ($x) mods)
-fastcatMod mods x = cat (fmap ($x) mods)
+patMod mods x = tt (recip . fromIntegral $ length mods) (catMod mods) x
+tt x f = (slow x) . f. (slow (1/x))
 
 prog :: Pattern ParamMap -> Pattern ParamMap
-prog = let (>>) = (>>>) in do
-  add $ ($ silence) $ do
-    const $ sound $ samples "tech*8" (run 8)
-    (# cut "8")
-    catMod
-      [ (|*| speed "1")
-      , (|*| speed "2")
-      ]
-    catMod
-      [ striate' 1 (1/4)
-      , striate' 1 1
-      ]
-    catMod
-      [ (|*| speed "1 1")
-      , (|*| speed "2 1")
-      , do
-        catMod
-          [ (|*| speed "4 2")
-          , (|*| speed "4 1")
-          ]
-      , (|*| speed "4 1")
-      ]
-    catMod
-      [ (# vowel "a e")
-      , (# vowel "e e e")
-      ]
-    (|*| speed "1 0.5")
-    catMod [id, id]
-
+prog = let (>>) = flip (.) in do
+  addMod $ do
+    const $ sound $ samples "dr55 ~ [dr55 dr55] dr55" "2"
+    (# gain "0.8")
+  jux $ ((2/4) ~>)
+  ((2/4) ~>)
+  -- catMod [ id , trunc 0.25 ]
+  -- catMod [ trunc 0.25, id ]
+  -- every 2 $ trunc 0.25 >>> (0.25 ~>)
+  -- tt 1 $ catMod [ id, ((4/8) ~>) ] >>> catMod [ ((4/8) ~>), id ] -- cool
+  addMod $ do
+    const $ sound $ samples "808bd" "1"
+    tt 2 $ catMod [slow (2/8), slow (3/8)]
+    (# gain "0.9")
+  whenmod 16 12 $ do
+    (slow 2)
+  catMod [id,  tt 1 (|*| speed "1 0 1 1")]
+  -- tt 0.5 $ catMod [ id, trunc 0.5 ] >>> catMod [ trunc 0.5, id ] -- cool
+  -- jux $ do
+  --   catMod [ id, (0.5 ~>) ] >>> catMod [ id, (0.5 <~) ]
+  --   catMod [ id, (0.5 ~>) ] >>> catMod [ id, (0.5 <~) ]
